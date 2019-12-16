07f301dbcae5b66d1fefd6702b2e48706111c9fb
Marian C. Moldovan <moldovan0991@gmail.com>
2019-12-17 00:40:08 +0100

    Added test for writer app

---
+++ writer/tests/test.js
@@ -0,0 +1,20 @@
+const { createReadStream } = require('fs')
+const { assert } = require('chai')
+const fetch = require('node-fetch')
+
+const app = require('../app.js')
+
+after(() => {
+  app.close()
+})
+
+describe('Test post endpoint', () => {
+  it('POST /upload should return 202', async () => {
+    const stream = createReadStream('sample.csv')
+    const response = await fetch('http://localhost:3000/upload', {
+      method: 'POST',
+      body: stream
+    })
+    assert.equal(response.status, 202)
+  })
+})
